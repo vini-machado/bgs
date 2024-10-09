b93c350c9b0c72c18b7af1ecd488acdc27a07ff3
vini-machado <viniciuscmachado22@gmail.com>
2024-10-09 11:26:17 -0300

    select more columns on SQL query

--- src/queries/measurements.sql
+++ src/queries/measurements.sql
@@ -1,13 +1,17 @@
 SELECT 
     tn.bwf_node_id,
     tn.bwf_object_id,
-    co.bwf_name      AS system_name, 
-    mo.bwf_name      AS measurement_name,
-    so.bwf_name      AS archive_name,
-    mo.bwf_meas_date AS measurement_date,
-    pt.bwf_depth     AS depth,
-    pt.bwf_pos_id    AS position_id,
-    rd.bwf_file_name AS data_file
+    co.bwf_name         AS system_name, 
+    mo.bwf_name         AS measurement_name,
+    so.bwf_name         AS archive_name,
+    mo.bwf_meas_date    AS measurement_date,
+    pt.bwf_depth        AS depth,
+    pt.bwf_pos_id       AS position_id,
+    pt.bwf_min_land     AS min_land, 
+    pt.bwf_limit_land   AS max_land, 
+    pt.bwf_min_groove   AS min_groove, 
+    pt.bwf_limit_groove AS max_groove, 
+    rd.bwf_file_name    AS data_file
 FROM (((((bwf_tree_node AS tn
     INNER JOIN bwf_b280_config_object AS co ON tn.bwf_object_id   = co.bwf_object_id)
     INNER JOIN bwf_b280_meas_object   AS mo ON tn.bwf_object_id   = mo.bwf_object_id)
